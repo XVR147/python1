0d61af8a48b2dc9d55b66deb607c43f70123a386
XVR147 <34273908+XVR147@users.noreply.github.com>
2017-12-23 15:01:51 +0100

    Updated insertionsort.py
    Formatting

--- Algorithms/insertionsort.py
+++ Algorithms/insertionsort.py
@@ -3,11 +3,13 @@
 unsortedArray = [2, 8, 9, 4, 3, 6, 12]
 print(unsortedArray)
 
+
 def swap(pos):
     tmp = unsortedArray[pos]
     unsortedArray[pos] = unsortedArray[pos+1]
     unsortedArray[pos+1] = tmp
 
+    
 for i in range(1,len(unsortedArray)-1):
     print("Ciclo ")
     j = i
